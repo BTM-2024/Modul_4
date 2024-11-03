d2becf3b175ba8f4f334145ae9c95a7e0c245cd8
BTM-2024 <btm_2000@mail.ru>
2024-11-03 19:14:04 +0300

    Add files via upload

---
+++ modul_4_2.py
@@ -0,0 +1,10 @@
+# Домашняя работа по уроку "Пространство имен."
+def test_function():
+
+    def inner_function():
+        a = "я в области видимоссти функции test_function"
+        print(a)
+    inner_function()
+
+test_function()
+
